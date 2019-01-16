b5d3d06d83d3b7172e055f458f0707e318013923
PerervaKostya <pererva.kostya@outlook.com>
2019-01-16 18:27:08 +0200

    верстка посадочной страницы

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+var swiperBrendLogo = new Swiper('.brand-gallery', {
+    slidesPerView: 5,
+    loop: false,
+    spaceBetween: 0,
+    scrollbar: {
+        el: '.brand-gallery__scrollbar',
+        draggable: true,
+    },
+});
