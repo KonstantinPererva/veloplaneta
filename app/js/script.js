8091ba1bc816b23b9412d75362d0f503d09d432a
PerervaKostya <pererva.kostya@outlook.com>
2019-01-17 18:31:25 +0200

    верстка посадочной страницы - mob

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,27 @@
 var swiperBrendLogo = new Swiper('.brand-gallery', {
-    slidesPerView: 5,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    initialSlide: 2,
     loop: false,
     spaceBetween: 0,
     scrollbar: {
         el: '.brand-gallery__scrollbar',
         draggable: true,
+        dragSize: 90
+    }
+});
+
+var swiperBrendDescr = new Swiper('.brand-info', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    freeMode: true,
+    scrollbar: {
+        el: '.brand-info__scrollbar',
+        snapOnRelease: true,
+    },
+    mousewheel: {
+        releaseOnEdges: true,
+        invert: false,
+        sensitivity: .1,
     },
 });
